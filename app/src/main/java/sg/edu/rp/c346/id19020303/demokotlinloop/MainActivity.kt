23b594683b2ea50188ed75a9330288a42b64b8d8
gab <19020303@myrp.edu.sg>
2021-08-12 16:28:34 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19020303/demokotlinloop/MainActivity.kt
@@ -0,0 +1,36 @@
+package sg.edu.rp.c346.id19020303.demokotlinloop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCount.setOnClickListener {
+
+            //for loop
+//            for(i in 1..5){
+//                Log.d("MainActivity", "$i")
+//            }
+
+            //while loop
+            var i = 1
+            while (i < 6){
+                Log.d("MainActivity", "$i")
+                i++
+            }
+        }
+
+        //getting letter by letter
+        btnLetter.setOnClickListener {
+            val word = etWord.text.toString()
+            for (letter in word){
+                Log.d("MainActivity", "$letter")
+            }
+        }
+    }
+}
